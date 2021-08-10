44a686ce5d0173adc7cfccafb5710e42d37fd2d8
Amit Nino <amitnino@moveo.co.il>
2021-08-10 18:07:07 +0300

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.5f;
+
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+    private void moveObject()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        if (System.Convert.ToBoolean(horizontalInput) || System.Convert.ToBoolean(verticalInput))
+        {
+            UnityEngine.Vector3 moveVector = new Vector3(horizontalInput, verticalInput, 0);
+            transform.Translate(moveVector * _speed * Time.deltaTime);
+        };
+    }
+    void Update()
+    {
+        moveObject();
+    }
+};
