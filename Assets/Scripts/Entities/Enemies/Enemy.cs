9fd937846141532c53a83c91573c27d68b28530f
Amit Nino <amitnino@moveo.co.il>
2021-08-16 11:13:22 +0300

    Added onDestroy logic

--- Assets/Scripts/Entities/Enemies/Enemy.cs
+++ Assets/Scripts/Entities/Enemies/Enemy.cs
@@ -4,23 +4,44 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    // Game OBJs
     [SerializeField]
     private GameObject _enemyPrefab;
     [SerializeField]
     private Player _player;
+    private Animator _animator;
+    private BoxCollider2D _boxCollider;
+    // GLOBAL
     private GlobalInfo globalInfo = GlobalInfo.Instance;
     [SerializeField]
+    // VARs
     private float _speed = 4f;
     public float Speed
     {
         get => _speed;
+        set
+        {
+            if (value < 0) return;
+            _speed = value;
+        }
     }
+    //CORE Methods
     private void Start()
     {
         _player = GameObject.Find("Player").GetComponent<Player>();
         if (_player.GetType() == null)
         {
-            Debug.Log("Enemy Component: Player is null");
+            Debug.Log("Enemy Component::Player is null");
+        }
+        _animator = GetComponent<Animator>();
+        if (_animator.GetType() == null)
+        {
+            Debug.Log("Enemy Component::Animator is null");
+        }
+        _boxCollider = GetComponent<BoxCollider2D>();
+        if (_animator.GetType() == null)
+        {
+            Debug.Log("Enemy Component::BoxCollider2D is null");
         }
     }
     private void OnTriggerEnter2D(Collider2D other)
@@ -29,13 +50,13 @@ public class Enemy : MonoBehaviour
         {
             _player.decreaseLife();
             _player.addScore(Random.Range(2, 5));
-            destroyAndInitNewEnemy();
+            DestroyEnemy();
             return;
         }
         else if (other.tag == "Projectiles")
         {
             _player.addScore(Random.Range(8, 15));
-            destroyAndInitNewEnemy();
+            DestroyEnemy();
             Destroy(other.gameObject);
         };
 
@@ -45,24 +66,19 @@ public class Enemy : MonoBehaviour
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
         wrapOnBottomScreen();
     }
+    void DestroyEnemy()
+    {
+        Speed = 1f;
+        _animator.SetTrigger("OnEnemyDeath");
+        _boxCollider.enabled = false;
+        Destroy(this.gameObject, 2.8f);
+    }
+    //Movement Methods
     void wrapOnBottomScreen()
     {
         if (transform.position.y < -7f)
         {
-            transform.position = getRandomVector();
+            transform.position = globalInfo.getRandomVectorEnemy();
         }
-    }
-    Vector3 getRandomVector()
-    {
-        float randomX = Random.Range(-1 * (globalInfo.XScreenBorder - 1), (globalInfo.XScreenBorder - 1));
-        return new Vector3(randomX, 7.5f, 0);
-    }
-    void destroyAndInitNewEnemy()
-    {
-        Destroy(this.gameObject);
-    }
-    public GameObject generateNewEnemy()
-    {
-        return Instantiate(_enemyPrefab, getRandomVector(), Quaternion.identity);
     }
 }
